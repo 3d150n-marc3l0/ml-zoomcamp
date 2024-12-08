1e391679eb5b411b97861e16b4d149ea5001afff
Edison Marcelo <edison.marcelo.muzo.oyana@gmail.com>
2024-12-08 23:48:27 +0100

    Add HW 09

---
+++ 09-serverless/lambda_function.py
@@ -0,0 +1,69 @@
+import tflite_runtime.interpreter as tflite
+from keras_image_helper import create_preprocessor
+from io import BytesIO
+from urllib import request
+from PIL import Image
+import numpy as np
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+tflite_model_path = 'model_2024_hairstyle_v2.tflite'
+target_size = (200, 200)
+
+# preprocessor = create_preprocessor('xception', target_size=target_size)
+
+interpreter = tflite.Interpreter(model_path=tflite_model_path)
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+classes = [
+    'curly',
+    'straight'
+]
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+
+def predict(url):
+    # X = preprocessor.from_url(url)
+    image = download_image(url)
+    image = prepare_image(image, target_size)
+    
+    x = np.array(image, dtype='float32')
+    X = np.expand_dims(x, axis=0) 
+    X = X / 255.0
+    
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    float_predictions = preds[0].tolist()
+
+    return dict(zip(classes, float_predictions))
+
+
+def lambda_handler(event, context):
+    url = event['url']
+    print(f"URL: {url}")
+    result = predict(url)
+    print(f"Result: {result}")
+    
+    return result
+    
